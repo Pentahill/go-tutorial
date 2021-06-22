ca5cf2a1f11676de6b5b3e03dfd1341877632f2c
liujinyao <liujinyao@sensetime.com>
2021-06-22 16:09:55 +0800

    feat: add websocket example

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/gocolly/colly v1.2.0
 	github.com/gocolly/colly/v2 v2.1.0
+	github.com/gorilla/websocket v1.4.2
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/marmotedu/errors v1.0.1
 	github.com/stretchr/testify v1.7.0
---
+++ websocket/main.go
@@ -0,0 +1,70 @@
+package websocket
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+func recieve(conn *websocket.Conn, reCh chan string, seCh chan string) {
+	for {
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println("receive error")
+		}
+
+		if msg != nil {
+			fmt.Println("receive msg ", string(msg))
+			reCh <- string(msg)
+		}
+	}
+}
+
+func send(conn *websocket.Conn, reCh chan string, seCh chan string) {
+	for {
+		select {
+		case msg := <-reCh:
+			conn.WriteMessage(websocket.TextMessage, []byte(msg))
+			fmt.Println("send message", msg)
+		}
+	}
+}
+
+func handleWS(w http.ResponseWriter, r *http.Request) {
+	conn, err1 := upgrader.Upgrade(w, r, nil)
+	if err1 != nil {
+		fmt.Println(err1)
+		w.Write([]byte("upgrade error"))
+		return
+	}
+
+	writer, err2 := conn.NextWriter(websocket.TextMessage)
+	if err2 != nil {
+		writer.Write([]byte("get writer error"))
+		return
+	}
+
+	receiveCh := make(chan string, 10)
+	sendCh := make(chan string)
+
+	go recieve(conn, receiveCh, sendCh)
+	go send(conn, receiveCh, sendCh)
+
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func main() {
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		handleWS(w, r)
+	})
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
